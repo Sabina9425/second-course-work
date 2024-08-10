b7b24e282e5668f496afce8ccfc07c5242ee51d8
Sabina9425 <visapa@icloud.com>
2024-08-10 23:06:48 +0400

    implement API for working with hh

--- src/api.py
+++ src/api.py
@@ -1,27 +1,48 @@
+from abc import abstractmethod
 import requests
+
 class Parser:
-    pass
-
-
-class HH(Parser):
-    """
-    Класс для работы с API HeadHunter
-    Класс Parser является родительским классом, который вам необходимо реализовать
-    """
-    def __init__(self, file_worker):
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
-        self.vacancies = []
-        super().__init__(file_worker)
-
-    def load_vacancies(self, keyword):
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
-            vacancies = response.json()['items']
-            self.vacancies.extend(vacancies)
-            self.params['page'] += 1
-class HeadHunterAPI:
-    def get_vacancies(self, param):
+    @abstractmethod
+    def _connect(self):
+        """Establish connection with the API."""
         pass
+
+    @abstractmethod
+    def get_vacancies(self, keyword: str):
+        """Fetch vacancies based on a search keyword."""
+        pass
+
+class HeadHunterAPI(Parser):
+    """
+    Class for access to HeadHunter API
+    """
+    def __init__(self):
+        self._url = 'https://api.hh.ru/vacancies'
+        self._headers = {'User-Agent': 'HH-User-Agent'}
+        self._params = {'text': '', 'page': 0, 'per_page': 100}
+        self._vacancies = []
+    def _connect(self):
+        """Establish connection to the hh.ru API by sending a basic request."""
+        response = requests.get(self._url, headers=self._headers)
+        if response.status_code == 200:
+            return True
+        else:
+            response.raise_for_status()
+
+    def get_vacancies(self, keyword: str):
+        """Fetch vacancies from hh.ru based on a search keyword."""
+        self._params['text'] = keyword
+        self._vacancies.clear()
+
+        self._connect()
+
+        while self._params['page'] < 20:  # Limit to 20 pages for this example
+            response = requests.get(self._url, headers=self._headers, params=self._params)
+            if response.status_code == 200:
+                data = response.json()
+                self._vacancies.extend(data.get('items', []))
+                self._params['page'] += 1
+            else:
+                break
+
+        return self._vacancies
